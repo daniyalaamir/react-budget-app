51dc13caeb7ab982575429280195f6c92488d4d7
Daniyal Aamir <daniyalaamir@daniyals-mbp.myfiosgateway.com>
2020-12-08 15:42:43 -0500

    create new components, add and remove entry from UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import { Container, Segment, Grid, Icon } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
-import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
+
+let initialEntries = [
+  {
+    id: 1,
+    description: "Work income",
+    value: "1000.00",
+    isExpense: false
+  },
+  {
+    id: 2,
+    description: "Water bill",
+    value: "20.00",
+    isExpense: true
+  },
+  {
+    id: 3,
+    description: "Rent",
+    value: "300",
+    isExpense: true
+  },
+  {
+    id: 4,
+    description: "Power bill",
+    value: "50",
+    isExpense: true
+  }
+]
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  const deleteEntry = id => {
+    const result = entries.filter(entry => entry.id !== id);
+    setEntries(result);
+  }
+
+  const addEntry = (description, value, isExpense) => {
+    const result = entries.concat({
+      id: entries.length + 1,
+      description,
+      value,
+      isExpense
+    })
+    setEntries(result)
+  }
+
   return (
     <Container>
 
@@ -15,11 +60,10 @@ function App() {
       <DisplayBalances />
 
       <MainHeader title="History" type="h3" />
-      <EntryLine description="income" value="10.00" />
-      <EntryLine description="expense" value="10.00" isExpense />
+      <EntryLines entries={entries} deleteEntry={deleteEntry} />
 
       <MainHeader title="Add new transaction" type="h3" />
-      <NewEntryForm />
+      <NewEntryForm addEntry={addEntry} />
 
     </Container>
   );
