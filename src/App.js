30259b45e471827c12f5e696a516f6e04827eecd
Daniyal Aamir <daniyalaamir@daniyals-mbp.myfiosgateway.com>
2020-12-08 18:43:23 -0500

    refactor components to manage the state and display it on the edit modal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
 import EntryLines from './components/EntryLines';
+import ModalEdit from './components/ModalEdit';
 
 let initialEntries = [
   {
@@ -36,6 +37,10 @@ let initialEntries = [
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [isExpense, setIsExpense] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const deleteEntry = id => {
     const result = entries.filter(entry => entry.id !== id);
@@ -52,6 +57,18 @@ function App() {
     setEntries(result)
   }
 
+  const editEntry = id => {
+    console.log("Edit entry with id= ", id);
+    if(id) {
+      const index = entries.findIndex(entry => entry.id === id);
+      const entry = entries[index];
+      setDescription(entry.description);
+      setValue(entry.value);
+      setIsExpense(entry.isExpense);
+      setIsOpen(true);
+    }
+  }
+
   return (
     <Container>
 
@@ -60,11 +77,34 @@ function App() {
       <DisplayBalances />
 
       <MainHeader title="History" type="h3" />
-      <EntryLines entries={entries} deleteEntry={deleteEntry} />
+      <EntryLines 
+        entries={entries} 
+        deleteEntry={deleteEntry}
+        editEntry={editEntry}
+      />
 
       <MainHeader title="Add new transaction" type="h3" />
-      <NewEntryForm addEntry={addEntry} />
+      <NewEntryForm 
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense} 
+      />
 
+      <ModalEdit 
+        isOpen={isOpen} 
+        setIsOpen={setIsOpen}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}  
+      />
+    
     </Container>
   );
 }
--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,9 +1,8 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { Segment, Grid, Icon } from 'semantic-ui-react'
 import ModalEdit from './ModalEdit'
 
-function EntryLine({ id, description, value, isExpense=false, deleteEntry }) {
-  const [isOpen, setIsOpen] = useState(false);
+function EntryLine({ id, description, value, isExpense=false, deleteEntry, editEntry }) {
   return (
     <Fragment>
       <Segment color={isExpense ? "red" : "green"}>
@@ -16,13 +15,12 @@ function EntryLine({ id, description, value, isExpense=false, deleteEntry }) {
               ${value}
             </Grid.Column>
             <Grid.Column width={3}>
-              <Icon name="edit" bordered onClick={() => setIsOpen(true)} />
+              <Icon name="edit" bordered onClick={() => editEntry(id)} />
               <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment>
-      <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen} />
     </Fragment>
   )
 }
